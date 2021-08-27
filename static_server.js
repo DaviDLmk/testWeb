ebd753bafded52e5609d2b8a31e6a329d1472447
DaviDLmk <litzlerdavid@gmail.com>
2021-08-27 17:54:12 +0200

    changement static_server

--- static_server.js
+++ static_server.js
@@ -4,11 +4,11 @@ const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/www'));
+app.use(express.static(__dirname + '/dist/frontEnd'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/www/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/frontEnd/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
